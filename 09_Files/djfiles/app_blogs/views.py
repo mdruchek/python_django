fe8f24c1a1bfb8687a7ed78acfb50b0d76c44532
Михаил Дручек <druchek_ma@vsw.ru>
2023-02-16 13:26:57 +0300

    Update 09_Files/djfiles/app_blogs/views.py, 09_Files/djfiles/app_blogs/templates/app_blogs/create.html, 09_Files/djfiles/app_blogs/templates/app_blogs/upload_file_csv.html, 09_Files/djfiles/app_users/templates/app_users/login.html, 09_Files/djfiles/app_blogs/urls.py files

--- 09_Files/djfiles/app_blogs/views.py
+++ 09_Files/djfiles/app_blogs/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.urls import reverse
+from django.views.generic import ListView, DetailView, FormMixin
 from django.views.generic.edit import CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Blog
-from .forms import BlogForm
+from .forms import UploadFileForm
 
 
 class BlogListView(ListView):
@@ -16,6 +17,26 @@ class BlogDetailView(DetailView):
     model = Blog
     context_object_name = 'blog'
 
-class CreateBlogView(LoginRequiredMixin, CreateView):
+
+class CreateBlogView(LoginRequiredMixin, CreateView, FormMixin):
     model = Blog
     fields = 'content', 'image
+
+
+def upload_blog_csv(request):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            csv_file = form.cleaned_data['file'].read()
+            csv_str = file.decode('utf-8').split('\n')
+            csv_reader = reader(csv_str, delimiter=',', quotechar='"')
+            for row in csv_reader:
+                blog = Blog.create(content=row[0], created=row[1])
+            return reverse('app_blogs:upload_file_csv')
+        else:
+            form=UploadFileForm()
+        context={
+            'form': form
+        }
+        return render(request, 'app_blogs/upload_blog_csv.html', context=context)
+
