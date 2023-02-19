66ee9526f2d7b1abdb7ed453dc9ac254691323b9
Mikhail Druchek <mdruchek@yandex.ru>
2023-02-19 21:48:51 +0300

    module09 update

--- 09_Files/djfiles/app_users/models.py
+++ 09_Files/djfiles/app_users/models.py
@@ -4,5 +4,5 @@ from django.contrib.auth.models import User
 
 
 class ProfileUser(models.Model):
-    user = models.ForeignKey(User, on_delete=CASCADE)
+    user = models.OneToOneField(User, on_delete=CASCADE)
     avatar = models.ImageField(upload_to='avatars/', blank=True)
--- 09_Files/djfiles/app_users/views.py
+++ 09_Files/djfiles/app_users/views.py
@@ -1,11 +1,15 @@
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.contrib.auth.views import LoginView, LogoutView
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.contrib.auth.models import User
+from .models import ProfileUser
 from django.views.generic import (UpdateView,
                                   CreateView)
 from .forms import RegistrationUserForm, UpdateUserForm
+from .models import ProfileUser
 
 
 class UsersLoginView(LoginView):
@@ -23,8 +27,28 @@ class UserCreateView(CreateView):
     template_name = 'app_users/create.html'
     success_url = reverse_lazy('app_blogs:list')
 
+
     def post(self, request, *args, **kwargs):
-        form =
+        form = self.get_form()
+        if form.is_valid():
+            user = form.save()
+            form.instace.avatar =ProfileUser(user=self.user, avatar=avatar)
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+
+    def form_valid(self, form):
+        if form.is_valid():
+
+        response = super().form_valid(form)
+        avatar = self.request.FILES.get('avatar')
+        if avatar:
+            form.instace.avatar = ProfileUser.objects.create(user=self.request.user, avatar=avatar)
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_date.get('password1')
+        user = authenticate(self.request, username=username, password=password)
+        login(self.request, user)
+        return response
 
 
 @login_required(login_url=reverse_lazy('app_users:login'))
